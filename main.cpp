22285b2998e4e28cd31f2064fc8bc0f129000ee8
Ryszard Sikora <ryszard.sikora7@gmail.com>
2017-06-23 15:43:47 +0200

    Added test cases and checking crystal positions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+namespace Sym {
+    static constexpr char Crystal = '*';
+    static constexpr char Blank = ' ';
+    static constexpr char Block = '#';
+    static constexpr char Mirror45 = '/';
+    static constexpr char Mirror135 = '\\';
+
+}
+
+struct Position {
+    unsigned x,y;
+
+    Position(unsigned int x, unsigned int y) : x(x), y(y) {}
+};
+
 struct MazeDescription {
     vector<vector<char>> maze;
+    vector<Position> crystalPositions;
     unsigned mirrors;
+
+    MazeDescription(const vector<vector<char>> &maze, const vector<Position> &crystalPositions, unsigned int mirrors)
+            : maze(maze), crystalPositions(crystalPositions), mirrors(mirrors) {}
 };
 
 void printSolution(const MazeDescription& mazeDescription) {
@@ -21,25 +41,34 @@ void printSolution(const MazeDescription& mazeDescription) {
 
 MazeDescription readInput() {
     vector<vector<char>> maze;
+    vector<Position> crystalPositions;
     unsigned height, width, mirrors;
 
     cin >> height >> width >> mirrors;
 
     maze.resize(height);
+    crystalPositions.reserve(mirrors); // It's just a guess
+    for_each(maze.begin(), maze.end(), [width](vector<char>& row) {row.resize(width);});
+
     cin.ignore(); // newline after mirrors
     for(size_t i = 0; i < height; ++i) {
-        maze[i].resize(width);
         for(size_t j = 0; j < width; ++j) {
             cin >> noskipws >> maze[i][j];
+            if (maze[i][j] == Sym::Crystal) {
+                crystalPositions.emplace_back(i, j);
+            }
         }
         cin.ignore(); // newline after a row
     }
 
-    return MazeDescription{maze, mirrors};
+    return MazeDescription{maze, crystalPositions, mirrors};
 }
 
 int main() {
     auto mazeDescription = readInput();
+
+
+
     cout << "Done\n";
     printSolution(mazeDescription);
 //    char c;
